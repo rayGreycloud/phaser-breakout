d89e2975c87cb8c4f31edaa838cd48b5ccf8d24b
rayGreycloud <rayGreycloud@gmail.com>
2017-07-22 13:18:09 -0700

    Add paddle

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var game = new Phaser.Game(width, height, Phaser.CANVAS, null, {
 });
 
 var ball;
+var paddle;
 
 function preload() {
   // Scale canvas but respect aspect ratio
@@ -20,24 +21,38 @@ function preload() {
 
   // Load ball image
   game.load.image('ball', '/assets/images/ball.png');
+  // Load paddle image
+  game.load.image('paddle', '/assets/images/paddle.png');
 }
 
 function create() {
   // Add physics
   game.physics.startSystem(Phaser.Physics.ARCADE);
   // Add ball
-  ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25, 'ball');
+  ball.anchor.set(0.5);
   // Enable physics on ball
   game.physics.enable(ball, Phaser.Physics.ARCADE);
   // Set velocity
-  ball.body.velocity.set(150, 150);
+  ball.body.velocity.set(150, -150);
   // Make ball bounce off world boundaries
   ball.body.collideWorldBounds = true;
   // Set ball bounce
   ball.body.bounce.set(1);
+  // Add paddle, position in middle
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+  // Set anchor
+  paddle.anchor.set(0.5,1);
+  // Enable physics on paddle
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  // Make paddle stay in place
+  paddle.body.immovable = true;
 
 }
 
 function update() {
-
+  // Enable collision btwn ball and paddle
+  game.physics.arcade.collide(ball, paddle);
+  // Set paddle control
+  paddle.x = game.input.x || game.world.width*0.5;
 }
