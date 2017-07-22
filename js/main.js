f7664ca25e62cdb3920ba0865257b053594b43a6
rayGreycloud <rayGreycloud@gmail.com>
2017-07-22 12:54:44 -0700

    Scale, position, color game canvas

--- js/main.js
+++ js/main.js
@@ -2,7 +2,25 @@ let width = 480;
 let height = 320;
 
 var game = new Phaser.Game(width, height, Phaser.CANVAS, null, {
-  preload: preload() {},
-  create: create() {},
-  update: update() {}
+  preload: preload,
+  create: create,
+  update: update
 });
+
+function preload() {
+  // Scale canvas but respect aspect ratio
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  // Center image
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  // Add custom canvas background color
+  game.stage.backgroundColor = '#eee';
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
